198f366c8c09f88b8a708e618e2e8aa3893fe001
anthony-kakatera <anthonyckakatera@gmail.com>
2023-10-31 04:02:11 +0200

    Added weighted edges and deleted previous implementation of graph

--- districts_graph_ins.py
+++ districts_graph_ins.py
@@ -5,32 +5,27 @@ class Districts:
         # Instance of WeightedGraph
         self.weighted_graph = WeightedGraph()
 
-# import networkx as nx
-
-# # empty non-directed graph
-# G = nx.Graph()
-
-# # Nodes
-# G.add_node('Mchinji')
-# G.add_node('Kasungu')
-# G.add_node('Lilongwe')
-# G.add_node('Dowa')
-# G.add_node('Ntchisi')
-# G.add_node('Dedza')
-# G.add_node('Salima')
-# G.add_node('Nkhotakota')
-# G.add_node('Ntcheu')
-
-# # Weighted edges
-# G.add_edge('Mchinji', 'Kasungu', weight=141)
-# G.add_edge('Mchinji', 'Lilongwe', weight=109)
-# G.add_edge('Kasungu', 'Dowa', weight=117)
-# G.add_edge('Kasungu', 'Ntchisi', weight=66)
-# G.add_edge('Lilongwe', 'Dowa', weight=55)
-# G.add_edge('Lilongwe', 'Dedza', weight=92)
-# G.add_edge('Dowa', 'Ntchisi', weight=38)
-# G.add_edge('Dowa', 'Salima', weight=67)
-# G.add_edge('Ntchisi', 'Nkhotakota', weight=66)
-# G.add_edge('Nkhotakota', 'Salima', weight=112)
-# G.add_edge('Salima', 'Dedza', weight=96)
-# G.add_edge('Dedza', 'Ntcheu', weight=74)
+    def use_weighted_graph(self):
+        # Nodes
+        self.weighted_graph.add_node('Mchinji')
+        self.weighted_graph.add_node('Kasungu')
+        self.weighted_graph.add_node('Lilongwe')
+        self.weighted_graph.add_node('Dowa')
+        self.weighted_graph.add_node('Ntchisi')
+        self.weighted_graph.add_node('Dedza')
+        self.weighted_graph.add_node('Salima')
+        self.weighted_graph.add_node('Nkhotakota')
+        self.weighted_graph.add_node('Ntcheu')
+        # Weighted edges
+        self.weighted_graph.add_edge('Mchinji', 'Kasungu', 141)
+        self.weighted_graph.add_edge('Mchinji', 'Lilongwe', 109)
+        self.weighted_graph.add_edge('Kasungu', 'Dowa', 117)
+        self.weighted_graph.add_edge('Kasungu', 'Ntchisi', 66)
+        self.weighted_graph.add_edge('Lilongwe', 'Dowa', 55)
+        self.weighted_graph.add_edge('Lilongwe', 'Dedza', 92)
+        self.weighted_graph.add_edge('Dowa', 'Ntchisi', 38)
+        self.weighted_graph.add_edge('Dowa', 'Salima', 67)
+        self.weighted_graph.add_edge('Ntchisi', 'Nkhotakota', 66)
+        self.weighted_graph.add_edge('Nkhotakota', 'Salima', 112)
+        self.weighted_graph.add_edge('Salima', 'Dedza', 96)
+        self.weighted_graph.add_edge('Dedza', 'Ntcheu', 74)
